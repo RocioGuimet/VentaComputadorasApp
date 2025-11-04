94abedce9089e6beb154393b1d4ce05638861f3b
unknown <rocioguimet@hotmail.com>
2025-11-04 19:09:36 -0300

    Agrego clase Monitor y creo objetos de prueba

--- VentaComputadorasApp/src/mundopc/Main.java
+++ VentaComputadorasApp/src/mundopc/Main.java
@@ -1,7 +1,17 @@
 package mundopc;
 
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
 public class Main {
     static void main(String[] args) {
-
+    // Crear objetos
+        Raton ratonLenovo = new Raton("bluetooth", "Lenovo");
+        System.out.println(ratonLenovo);
+        Teclado tecladoLenovo = new Teclado("bluetooth", "Lenovo");
+        System.out.println(tecladoLenovo);
+        Monitor monitorLenovo = new Monitor("Lenovo", (double) 27);
+        System.out.println(monitorLenovo);
     }
 }
---
+++ VentaComputadorasApp/src/mundopc/modelo/Monitor.java
@@ -0,0 +1,28 @@
+package mundopc.modelo;
+
+public class Monitor {
+    private final int idMonitor; //Creamos un ID por cada monitor
+    private String marca;
+    private double tamanio;
+    private static int contadorMonitores;
+
+    // Creamos constructor
+    private Monitor(){
+        idMonitor = ++contadorMonitores;
+    }
+
+    public Monitor(String marca, Double tamanio){
+        this(); //Llamamos al constructor
+        this.marca = marca;
+        this.tamanio = tamanio;
+    }
+
+    @Override
+    public String toString() {
+        return "Monitor{" +
+                "idMonitor=" + idMonitor +
+                ", marca='" + marca + '\'' +
+                ", tamanio=" + tamanio +
+                '}';
+    }
+}
